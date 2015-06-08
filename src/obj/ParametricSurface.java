b89692f84ea9013bb30c83c21aac4509ed4a667b
imrayya <imrayya@gmail.com>
2015-06-08 15:07:48 +0200

    Added Parametric Surface

--- src/obj/ParametricSurface.java
+++ src/obj/ParametricSurface.java
@@ -1,10 +1,35 @@
 package obj;
 
+import java.util.List;
+import obj.parametric_formula.FormulaAbstract;
+
 /**
  *
  * @author Kareem Horstink
  */
-public class ParametricSurface {
+public class ParametricSurface extends Object3D {
+
+    private FormulaAbstract x;
+    private FormulaAbstract y;
+    private FormulaAbstract z;
+
+    ;
+
+    public ParametricSurface(FormulaAbstract x, FormulaAbstract y, FormulaAbstract z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public List<Vertex> getVerts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Triangle> getTris() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
 
