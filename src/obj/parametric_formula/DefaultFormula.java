be590e56faae468b22bac71087bd6cd9a193eac4
imrayya <imrayya@gmail.com>
2015-06-08 15:04:10 +0200

    Added Sphere paramatric

--- src/obj/parametric_formula/DefaultFormula.java
+++
@@ -1,9 +0,0 @@
-package obj.parametric_formula;
-
-/**
- *
- * @author Kareem Horstink
- */
-public class DefaultFormula extends FormulaAbstract{
-    
-}
---
+++ src/obj/parametric_formula/SphereX.java
@@ -0,0 +1,14 @@
+package obj.parametric_formula;
+
+/**
+ *
+ * @author Kareem Horstink
+ */
+public class SphereX extends FormulaAbstract{
+
+    @Override
+    public double mumble(double u, double v) {
+        return Math.cos(u)*Math.sin(v);
+    }
+    
+}
