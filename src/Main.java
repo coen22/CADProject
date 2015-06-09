4815c21834b75a19677c7741342e7e957a88ad17
davidr88 <david.roschewitz@gmail.com>
2015-06-09 16:52:02 +0200

    bunny and stuff working

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import obj.Mesh;
 import obj.Tetrahedron;
 import obj.Triangle;
 import obj.Vertex;
@@ -11,13 +12,17 @@ public class Main {
 		
 		MainFrame frame = new MainFrame();
 		
-		Vertex v1 = new Vertex(0.0, 0.0, 0.0);
-		Vertex v2 = new Vertex(0.0, 0.0, 1.0);
-		Vertex v3 = new Vertex(1.0, 0.0, 0.0);
-		Vertex v4 = new Vertex(0.5, 1.0, 0.5);
-		Tetrahedron tetra = new Tetrahedron(v1, v2, v3, v4);
+//		Vertex v1 = new Vertex(0.0, 0.0, 0.0);
+//		Vertex v2 = new Vertex(0.0, 0.0, 1.0);
+//		Vertex v3 = new Vertex(1.0, 0.0, 0.0);
+//		Vertex v4 = new Vertex(0.5, 1.0, 0.5);
+//		Tetrahedron tetra = new Tetrahedron(v1, v2, v3, v4);
+//		
+//		frame.setObject(tetra);
 		
-		frame.setObject(tetra);
+		Mesh bunny = new Mesh("C:\\Users\\David Admin\\Documents\\DKE\\Period 6\\CADProject\\obj\\bunny2.obj");
+		
+		frame.setObject(bunny);
 	}
 
 }
