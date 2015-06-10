74740952cc840351c8777cc330fd2add986adc8b
imrayya <imrayya@gmail.com>
2015-06-10 11:41:00 +0200

    Parametric Surface

--- src/obj/ParametricSurface.java
+++ src/obj/ParametricSurface.java
@@ -15,6 +15,7 @@ import obj.parametric_formula.TorusZ;
 import obj.parametric_formula.TrumpetX;
 import obj.parametric_formula.TrumpetY;
 import obj.parametric_formula.TrumpetZ;
+import ui.MainFrame;
 
 /**
  *
@@ -33,8 +34,13 @@ public class ParametricSurface extends Object3D {
     private double interval = Math.PI / 30;
 
     public static void main(String[] args) {
-        ParametricSurface p = new ParametricSurface(new TorusX(), new TorusY(), new TorusZ());
-        p.test();
+        ParametricSurface p = new ParametricSurface(new SphereX(), new SphereY(), new SphereZ());
+
+//        p.test();
+        MainFrame m = new MainFrame();
+        Mesh me = new Mesh("C:\\Users\\Kareem\\Desktop\\bunny.obj");
+//        m.setObject(me);
+        m.setObject(p);
     }
 
     private void test() {
@@ -109,16 +115,109 @@ public class ParametricSurface extends Object3D {
         this.x = x;
         this.y = y;
         this.z = z;
+        stepNumberU = (int) (Math.abs(limitMinU - limitMaxU) / interval);
+        stepNumberV = (int) (Math.abs(limitMinV - limitMaxV) / interval);
+        vertexIndex = new int[stepNumberU + 1][stepNumberV + 1];
+        vertex = new double[stepNumberU + 1][stepNumberV + 1][3];
+
+        int count = 0;
+        int counter = 0;
+        for (double u = limitMinU; u <= limitMaxU; u += interval) {
+            int counter2 = 0;
+            for (double v = limitMinV; v <= limitMaxV; v += interval) {
+                count++;
+                vertexIndex[counter][counter2] = count;
+                vertex[counter][counter2][0] = x.mumble(u, v);
+                vertex[counter][counter2][1] = y.mumble(u, v);
+                vertex[counter][counter2][2] = z.mumble(u, v);
+                counter2++;
+            }
+            counter++;
+        }
     }
+    int stepNumberU;
+    int stepNumberV;
+    double[][][] vertex;
+    int[][] vertexIndex;
 
     @Override
     public List<Vertex> getVerts() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ArrayList<int[]> face = new ArrayList<>();
+
+        int count = 0;
+        int counter = 0;
+        for (double u = limitMinU; u <= limitMaxU; u += interval) {
+            int counter2 = 0;
+            for (double v = limitMinV; v <= limitMaxV; v += interval) {
+                count++;
+                vertexIndex[counter][counter2] = count;
+                vertex[counter][counter2][0] = x.mumble(u, v);
+                vertex[counter][counter2][1] = y.mumble(u, v);
+                vertex[counter][counter2][2] = z.mumble(u, v);
+                counter2++;
+            }
+            counter++;
+        }
+
+        for (int i = 0; i < vertex.length; i++) {
+            for (int j = 0; j < vertex[0].length; j++) {
+                if (i == vertex.length - 1 && j == vertex[0].length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[0][j], vertexIndex[i][0]});
+                    face.add(new int[]{vertexIndex[0][j], vertexIndex[0][0], vertexIndex[i][0]});
+                } else if (j == vertex[0].length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[i + 1][j], vertexIndex[i][0]});
+                    face.add(new int[]{vertexIndex[i + 1][j], vertexIndex[i + 1][0], vertexIndex[i][0]});
+                } else if (i == vertex.length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[0][j], vertexIndex[i][j + 1]});
+                    face.add(new int[]{vertexIndex[0][j], vertexIndex[0][j + 1], vertexIndex[i][j + 1]});
+                } else {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[i + 1][j], vertexIndex[i][j + 1]});
+                    face.add(new int[]{vertexIndex[i + 1][j], vertexIndex[i + 1][j + 1], vertexIndex[i][j + 1]});
+                }
+            }
+        }
+        return null;
     }
 
     @Override
     public List<Triangle> getTris() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        ArrayList<int[]> face = new ArrayList<>();
+
+        for (int i = 0; i < vertex.length; i++) {
+            for (int j = 0; j < vertex[0].length; j++) {
+                if (i == vertex.length - 1 && j == vertex[0].length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[0][j], vertexIndex[i][0]});
+                    face.add(new int[]{vertexIndex[0][j], vertexIndex[0][0], vertexIndex[i][0]});
+                } else if (j == vertex[0].length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[i + 1][j], vertexIndex[i][0]});
+                    face.add(new int[]{vertexIndex[i + 1][j], vertexIndex[i + 1][0], vertexIndex[i][0]});
+                } else if (i == vertex.length - 1) {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[0][j], vertexIndex[i][j + 1]});
+                    face.add(new int[]{vertexIndex[0][j], vertexIndex[0][j + 1], vertexIndex[i][j + 1]});
+                } else {
+                    face.add(new int[]{vertexIndex[i][j], vertexIndex[i + 1][j], vertexIndex[i][j + 1]});
+                    face.add(new int[]{vertexIndex[i + 1][j], vertexIndex[i + 1][j + 1], vertexIndex[i][j + 1]});
+                }
+            }
+        }
+        ArrayList<Triangle> tri = new ArrayList<>();
+        ArrayList<Vertex> ver = new ArrayList<>();
+        for (int i = 0; i < stepNumberU; i++) {
+            for (int j = 0; j < stepNumberV; j++) {
+                ver.add(new Vertex(vertex[i][j][0], vertex[i][j][1], vertex[i][j][2]));
+            }
+        }
+        int tmp = 0;
+        for (int[] face1 : face) {
+            tmp++;
+            if (tmp <= 3600) {
+                tri.add(new Triangle(ver.get(face1[0]), ver.get(face1[1]), ver.get(face1[2])));
+            }
+        }
+        System.out.println("face " + face.size());
+        System.out.println("tri " + tri.size());
+        return tri;
     }
 
 }
