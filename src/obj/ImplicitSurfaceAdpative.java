232879a475968140a0e06cd0489684be05b0653b
imrayya <imrayya@gmail.com>
2015-06-15 12:08:53 +0200

    Got rid of the main

--- src/obj/ImplicitSurfaceAdpative.java
+++ src/obj/ImplicitSurfaceAdpative.java
@@ -18,21 +18,16 @@ public class ImplicitSurfaceAdpative extends Object3D {
     private ArrayList<double[]> normal;
     private double[][][] listOfChecked;
 
-    public static void main(String[] args) {
-        ImplicitSurfaceAdpative i = new ImplicitSurfaceAdpative(new Torus(), 0.03125, 2);
-//        MainFrame m = new MainFrame();
-//        m.setObject(i);
-    }
-
+   
     public ImplicitSurfaceAdpative(FormulaAbstract formula, double interval, double checkSize) {
         this.formula = formula;
 
         numberOfChecks = (int) (checkSize * 2 / interval);
         listOfChecked = new double[numberOfChecks][numberOfChecks][numberOfChecks];
-        
+
         face = new ArrayList<>();
         points = new ArrayList<>();
-        
+
         boolean solved = false;
         double intervalAct = checkSize;
 
@@ -41,18 +36,22 @@ public class ImplicitSurfaceAdpative extends Object3D {
             if (face.isEmpty() && intervalAct == interval) {
                 throw new Error("Could not add any voxel \n Check if there is a formula inputted or if the check size is correct");
             }
-            
+
         }
     }
 
     int numberOfChecks;
 
-    public double checkInsideV2(double x, double z, double y) {
+    private boolean checkInsideV3(double x, double z, double y) {
+        return checkInsideV2(x, z, y) < 0;
+    }
+
+    private double checkInsideV2(double x, double z, double y) {
 
         if (listOfChecked[(int) x * numberOfChecks][(int) y * numberOfChecks][(int) z * numberOfChecks] != Double.NaN) {
             return listOfChecked[(int) x * numberOfChecks][(int) y * numberOfChecks][(int) z * numberOfChecks];
         } else {
-            double tmp = checkInside(x, y, z);
+            double tmp = checkInsideV1(x, y, z);
             listOfChecked[(int) x * numberOfChecks][(int) y * numberOfChecks][(int) z * numberOfChecks] = tmp;
             return tmp;
         }
@@ -63,7 +62,7 @@ public class ImplicitSurfaceAdpative extends Object3D {
         return (0 == formula.evaluateAt(x, y, z));
     }
 
-    private double checkInside(double x, double y, double z) {
+    private double checkInsideV1(double x, double y, double z) {
         return (formula.evaluateAt(x, y, z));
     }
 
