ebe74bd08f2358167d82fdbcda584cf2404678c7
imrayya <imrayya@gmail.com>
2015-06-15 11:39:17 +0200

    Implicit surface change

--- src/obj/ImplicitSurface.java
+++ src/obj/ImplicitSurface.java
@@ -1,20 +1,9 @@
 package obj;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import obj.implicit_formula.Diamond;
 import obj.implicit_formula.FormulaAbstract;
-import obj.implicit_formula.Genus2;
-import obj.implicit_formula.Neovius;
-import obj.implicit_formula.Sphere;
-import obj.implicit_formula.SumOfSins;
-import obj.implicit_formula.SwissCube;
 import obj.implicit_formula.Torus;
-import obj.implicit_formula.TorusCube;
-import obj.implicit_formula.TorusIntersectSphere;
 import ui.MainFrame;
 
 /**
@@ -29,18 +18,53 @@ public class ImplicitSurface extends Object3D {
     private ArrayList<double[]> normal;
 
     public static void main(String[] args) {
-        ImplicitSurface i = new ImplicitSurface(new Sphere(), 0.05, 1);
+        ImplicitSurface i = new ImplicitSurface(new Torus(), 0.05, 2);
         MainFrame m = new MainFrame();
         m.setObject(i);
     }
 
+    /**
+     * None voxel version
+     *
+     * @param formula
+     * @param interval
+     * @param checkSize
+     */
     public ImplicitSurface(FormulaAbstract formula, double interval, double checkSize) {
+        this.formula = formula;
+
+        face = null;
+        points = new ArrayList<>();
+
+        System.out.println("Number of function evalution to do " + Math.pow(checkSize / interval, 3));
+        //creates the vertices
+
+        for (double x = -checkSize; x < checkSize; x += interval) {
+            for (double y = -checkSize; y < checkSize; y += interval) {
+                for (double z = -checkSize; z < checkSize; z += interval) {
+                    if (checkInside(x, y, z)) {
+                        points.add(new Vertex(x, y, z, new double[]{partX(x, y, z, interval), partY(x, y, z, interval), partZ(x, y, z, interval)}));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Use this one for a voxel one
+     *
+     * @param formula
+     * @param interval
+     * @param checkSize
+     * @param voxel
+     */
+    public ImplicitSurface(FormulaAbstract formula, double interval, double checkSize, int voxel) {
         this.formula = formula;
 
         face = new ArrayList<>();
         points = new ArrayList<>();
-        
-        System.out.println("Number of function evalution to do " + Math.pow(checkSize/interval, 3));
+
+        System.out.println("Number of function evalution to do " + Math.pow(checkSize / interval, 3));
         //creates the vertices
 
         for (double x = -checkSize; x < checkSize; x += interval) {
@@ -57,8 +81,6 @@ public class ImplicitSurface extends Object3D {
                 }
             }
         }
-
-
     }
 
     private boolean checkOnSurface(double x, double y, double z) {
