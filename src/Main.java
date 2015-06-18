30f85e8b0f3c39cc1143344dad7c1a5c4cef8710
Coen Hacking <coen.hacking@gmail.com>
2015-06-18 10:26:53 +0200

    removed unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,4 @@
-import obj.ImplicitSurface;
-import obj.Mesh;
-import obj.ParametricSurface;
-import obj.Tetrahedron;
-import obj.Vertex;
-import obj.implicit_formula.Genus2;
-import obj.parametric_formula.TorusX;
-import obj.parametric_formula.TorusY;
-import obj.parametric_formula.TorusZ;
 import ui.Controller;
-import ui.MainFrame;
 
 public class Main {
 
